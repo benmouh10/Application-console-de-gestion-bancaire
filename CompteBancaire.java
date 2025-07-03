523b7b1e6a13e8be8d64c88b3cbe8a2bbfab4ba3
benmouh10 <mohamedbenmouh91@gmail.com>
2025-07-03 10:23:35 +0100

    Add files via upload

---
+++ CompteBancaire.java
@@ -0,0 +1,13 @@
+package com.votreprenom.banque.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompteBancaire {
+    private String numero;
+    private String titulaire;
+    private double solde;
+    private List<Transaction> transactions;
+    
+    // Constructeurs, getters, setters et méthodes métier
+}
